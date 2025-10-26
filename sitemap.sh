2654d7730d8afc1b876f71c2667c37f9f0011508
M.Bascoy <MBascoyP@gmail.com>
2025-10-26 15:09:02 +0100

    add sitemap for google search console

---
+++ sitemap.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# url configuration
+URL="https://mbascoy.github.io/"
+
+# values: always hourly daily weekly monthly yearly never
+FREQ="monthly"
+
+# begin new sitemap
+exec 1> sitemap.xml
+
+# print head
+echo '<?xml version="1.0" encoding="UTF-8"?>'
+echo '<!-- generator="Milkys Sitemap Generator, https://github.com/mcmilk/sitemap-generator" -->'
+echo '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">'
+
+# print urls
+IFS=$'\r\n' GLOBIGNORE='*' command eval "OPTIONS=($(cat $0.options))"
+find . -type f "${OPTIONS[@]}" -printf "%TY-%Tm-%Td%p\n" | \
+while read -r line; do
+  DATE=${line:0:10}
+  FILE=${line:12}
+  echo "<url>"
+  echo " <loc>${URL}${FILE}</loc>"
+  echo " <lastmod>$DATE</lastmod>"
+  echo " <changefreq>$FREQ</changefreq>"
+  echo "</url>"
+done
+
+# print foot
+echo "</urlset>"
